f661e4a173b17e239d5cb6b005f18e2e8bb34441
FredrikGhofran <fredrik_ghofran@hotmail.com>
2014-05-07 15:57:10 +0200

    Initial Commit

---
+++ FoodApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FoodApp
+//
+//  Created by Fredrik Ghofran on 2014-05-07.
+//  Copyright (c) 2014 Fredrik Ghofran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FoodApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FoodApp
+//
+//  Created by Fredrik Ghofran on 2014-05-07.
+//  Copyright (c) 2014 Fredrik Ghofran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
